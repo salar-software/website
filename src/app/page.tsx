372386781ae75a5b4e17a5e5759499dc5fd879fa
Quentin Wright <wrightq00@gmail.com>
2025-08-20 17:00:16 -0400

    fix: change company name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-3 gap-16">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl">salar</h1>
+        <h1 className="text-4xl">salar software</h1>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center text-xs">
         <p>&copy; 2025 Salar Software LLC</p>
